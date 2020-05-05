9fb932912d28e168ae613ac44165e58cc4820ad2
Vadim <vdzerniuk@gmail.com>
2020-05-05 23:02:19 +0300

    Add authorization

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,71 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const logInInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+
+let login = localStorage.getItem('acc');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function authorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem('acc');
+
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+
+  function logIn(event) {
+    event.preventDefault();
+    login = logInInput.value;
+
+    localStorage.setItem('acc', login);
+
+    toggleModalAuth();
+    buttonAuth.removeEventListener("click", toggleModalAuth);
+    closeAuth.removeEventListener("click", toggleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
